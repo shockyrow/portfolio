a41f4abd0743187dd7695c15736ed52201f7b475
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2019-03-27 00:19:13 +0300

    Updated About component

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TheGuy from '../../storage/img/the_man.jpg';
+import '../../css/About.css';
 import moment from 'moment';
 
 class About extends Component {
@@ -11,16 +12,19 @@ class About extends Component {
                 <div className="col-sm-6">
                     <img src={TheGuy} className="w-100"/>
                 </div>
-                <div className="col d-flex flex-column justify-content-center align-items-center">
-                    <div className="display-1">
-                        Hello!
-                    </div>
-                    <h2>
+
+                <div className="col d-flex flex-column justify-content-center align-items-center text-center">
+                    <div className="display-4 corben-font font-weight-bold">
                         IBROKHIM SHOKIROV
-                    </h2>
-                    <h5>
+                    </div>
+
+                    <h4 className="pacifico-font text-warning my-3">
                         Computer Engineer
-                    </h5>
+                    </h4>
+
+                    <button className="btn btn-lg btn-dark my-2">
+                        Download CV
+                    </button>
                 </div>
             </div>
         );
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Logo from '../../storage/img/is_logo.png';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-white shadow rounded">
-                <a className="navbar-brand" href="#">Ibrokhim Shokirov</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow rounded">
+                <a className="navbar-brand" href="#">
+                    <img src={Logo} width="30" height="30" className="bg-dark rounded p-2" alt=""/>
+                </a>
+
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -14,11 +18,11 @@ class Navbar extends Component {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                        {/*<li className="nav-item active">
+                        <li className="nav-item active">
                             <a className="nav-link" href="#">
                                 Home
                             </a>
-                        </li>*/}
+                        </li>
                     </ul>
                 </div>
             </nav>
