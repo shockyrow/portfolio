6809d1272398465ad952ee4981c7030bd9bfc65e
Ibrokhim Shokirov <i.shokirov@paysera.com>
2019-03-27 15:21:50 +0300

    Template added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './js/App';
-import * as serviceWorker from './serviceWorker';
-import 'jquery/dist/jquery.min';
-import 'popper.js/dist/popper.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
