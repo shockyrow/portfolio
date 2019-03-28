2af0c4d050a64ec739f69171323f6def3577a59d
Ibrokhim Shokirov <i.shokirov@paysera.com>
2019-03-28 12:10:27 +0300

    Started recreating design

--- src/js/App.js
+++ src/js/App.js
@@ -1,21 +1,10 @@
 import React, {Component} from 'react';
 import '../css/App.scss';
-import Header from "./components/Header";
-import Welcome from "./components/Welcome";
-import About from "./components/About";
-import Skills from "./components/Skills";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Header/>
-
-                <div className="container">
-                    <Welcome/>
-                    <About/>
-                    <Skills/>
-                </div>
             </div>
         );
     }
