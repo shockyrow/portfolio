db4b4c33b8efac4bffb071b9c27356f9ed27a040
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2019-04-03 13:52:28 +0300

    Removed carousels

--- src/js/components/Educations.js
+++ src/js/components/Educations.js
@@ -10,66 +10,50 @@ class Educations extends Component {
             <Fragment>
                 <InfoBlock title={"Educations"} content={(
                     <Fragment>
-                        <div id="educationCarousel" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <div>
-                                        <h6 className="pacifico-font">
-                                            High School
-                                        </h6>
-                                        <h4 className="opensans-title text-left shadowed-text">
-                                            Lyceum for Gifted Students in Dushanbe
-                                        </h4>
-                                        <h6 className="d-flex opensans-title m-0">
-                                            <div>
-                                                GPA:
-                                                <div className="text-muted">5.0/5</div>
-                                            </div>
-
-                                            <div className="px-2">
-                                                Duration:
-                                                <div className="text-muted">2008 - 2013</div>
-                                            </div>
-                                        </h6>
-                                        <h5 className="pacifico-font text-muted">
-
-                                        </h5>
-                                        <h5>
-                                            My high school life was full of social and educational activities. Some of my
-                                            achievements from these activities are listed in the achievement section below.
-                                        </h5>
-                                    </div>
+                        <div>
+                            <h6 className="pacifico-font">
+                                Bachelor of Science Degree
+                                <span className="text-muted pl-2">2014 - 2018</span>
+                            </h6>
+                            <h4 className="opensans-title text-left shadowed-text">
+                                Computer Engineering at Gazi University
+                            </h4>
+                            <h6 className="d-flex opensans-title">
+                                <div>
+                                    GPA:
+                                    <span className="text-muted pl-2">2.85/4</span>
                                 </div>
-                                <div className="carousel-item">
-                                    <div>
-                                        <h6 className="pacifico-font">
-                                            Bachelor’s Degree
-                                        </h6>
-                                        <h4 className="opensans-title text-left shadowed-text">
-                                            Computer Engineering at Gazi University
-                                        </h4>
-                                        <h6 className="d-flex opensans-title">
-                                            <div>
-                                                GPA:
-                                                <div className="text-muted">2.85/4</div>
-                                            </div>
+                            </h6>
+                            <h5>
+                                My university life had little less social activities so I had more time to develop
+                                myself.
+                            </h5>
+                        </div>
 
-                                            <div className="px-2">
-                                                Duration:
-                                                <div className="text-muted">2014 - 2018</div>
-                                            </div>
-                                        </h6>
-                                        <h5>
-                                            My university life had little less social activities so I had more time to develop
-                                            myself.
-                                        </h5>
-                                    </div>
+                        <div className="dropdown-divider my-3">
+                        </div>
+
+                        <div>
+                            <h6 className="pacifico-font">
+                                High School
+                                <span className="text-muted pl-2">2008 - 2013</span>
+                            </h6>
+                            <h4 className="opensans-title text-left shadowed-text">
+                                Lyceum for Gifted Students in Dushanbe
+                            </h4>
+                            <h6 className="d-flex opensans-title m-0">
+                                <div>
+                                    GPA:
+                                    <span className="text-muted pl-2">5.0/5</span>
                                 </div>
-                            </div>
-                            <ol className="carousel-indicators position-static mb-0">
-                                <li data-target="#educationCarousel" data-slide-to="0" className="custom-indicator active"></li>
-                                <li data-target="#educationCarousel" data-slide-to="1" className="custom-indicator"></li>
-                            </ol>
+                            </h6>
+                            <h5 className="pacifico-font text-muted">
+
+                            </h5>
+                            <h5>
+                                My high school life was full of social and educational activities. Some of my
+                                achievements from these activities are listed in the achievement section below.
+                            </h5>
                         </div>
                     </Fragment>
                 )}/>
--- src/js/components/Experiences.js
+++ src/js/components/Experiences.js
@@ -10,102 +10,73 @@ class Experiences extends Component {
             <Fragment>
                 <InfoBlock title={"Experiences"} content={(
                     <Fragment>
-                        <div id="experienceCarousel" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <div>
-                                        <h6 className="pacifico-font">
-                                            C# Developer
-                                        </h6>
-                                        <h4 className="opensans-title text-left shadowed-text">
-                                            İM PARK EĞİTİM BİLİŞİM LTD. ŞTİ.
-                                        </h4>
-                                        <h6 className="d-flex opensans-title text-muted m-0">
-                                            JANUARY 2016 - MAY 2016
-                                        </h6>
-                                        <h5 className="pacifico-font text-muted">
+                        <div>
+                            <h6 className="pacifico-font">
+                                Paysera
+                            </h6>
+                            <h4 className="opensans-title text-left shadowed-text">
+                                PhP Developer
+                            </h4>
+                            <h6 className="d-flex opensans-title text-muted">
+                                2018 - Now
+                            </h6>
+                            <h5>
+                                Description and responsibilities here!
+                            </h5>
+                        </div>
 
-                                        </h5>
-                                        <h5>
-                                            Started as an intern and later was offered a part-time job. I was one of the
-                                            developers of the educational web project that aimed to teach foreigners
-                                            Turkish language. I was programming with C# using MVC framework. I only left
-                                            this job because I needed to focus on my education.
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div className="carousel-item">
-                                    <div>
-                                        <h6 className="pacifico-font">
-                                            Java Developer
-                                        </h6>
-                                        <h4 className="opensans-title text-left shadowed-text">
-                                            PTT GENEL MÜDÜRLÜĞÜ
-                                        </h4>
-                                        <h6 className="d-flex opensans-title text-muted m-0">
-                                            SUMMER 2016
-                                        </h6>
-                                        <h5>
-                                            I did an internship at PTT’s Project Management Department as a Java
-                                            Developer. Here I created a desktop application which was able to manage all
-                                            the projects that company had. The application was based on basic management
-                                            applications.
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div className="carousel-item">
-                                    <div>
-                                        <h6 className="pacifico-font">
-                                            Security Analyst / Secure Developer
-                                        </h6>
-                                        <h4 className="opensans-title text-left shadowed-text">
-                                            BEAM TEKNOLOJİ A. Ş.
-                                        </h4>
-                                        <h6 className="d-flex opensans-title text-muted m-0">
-                                            2017 - 2018
-                                        </h6>
-                                        <h5>
-                                            Started as an intern and later was offered a part-time job. Had vocation for
-                                            about 3 months because of my thesis. Last 5 months I was working full-time.
-                                            I was responsible for development and maintenance of the phishing project
-                                            that company owned. As a secure developer I had to consider security issues
-                                            that the application could go through and fix them before the deployment of
-                                            the application and also add features to the existing application. As a Web
-                                            Security Analyst I have had 3 projects to test for security vulnerabilities
-                                            and report them with solutions. These projects belonged to some of the
-                                            biggest companies in Turkey.
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div className="carousel-item">
-                                    <div>
-                                        <h6 className="pacifico-font">
-                                            PhP Developer
-                                        </h6>
-                                        <h4 className="opensans-title text-left shadowed-text">
-                                            Paysera
-                                        </h4>
-                                        <h6 className="d-flex opensans-title text-muted m-0">
-                                            2018 - Now
-                                        </h6>
-                                        <h5>
-                                            Responsible for implementing gateways for company partners. Implementation
-                                            not only must be according to the company's code style, but also should
-                                            secure and safe. These gateways are implemented inside the main project at
-                                            first and later when ready are separated into libraries. These libraries
-                                            also contain their own unit and functional tests.
-                                        </h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <ol className="carousel-indicators position-static mb-0">
-                                <li data-target="#experienceCarousel" data-slide-to="0"
-                                    className="custom-indicator active"></li>
-                                <li data-target="#experienceCarousel" data-slide-to="1"
-                                    className="custom-indicator"></li>
-                                <li data-target="#experienceCarousel" data-slide-to="2"
-                                    className="custom-indicator"></li>
-                            </ol>
+                        <div className="dropdown-divider my-3">
+                        </div>
+
+                        <div>
+                            <h6 className="pacifico-font">
+                                BEAM Teknoloji A. Ş.
+                            </h6>
+                            <h4 className="opensans-title text-left shadowed-text">
+                                Web Security Analyst & Secure Developer
+                            </h4>
+                            <h6 className="d-flex opensans-title text-muted">
+                                2017 - 2018
+                            </h6>
+                            <h5>
+                                Description and responsibilities here!
+                            </h5>
+                        </div>
+
+                        <div className="dropdown-divider my-3">
+                        </div>
+
+                        <div>
+                            <h6 className="pacifico-font">
+                                PTT Genel Müdürlüğü
+                            </h6>
+                            <h4 className="opensans-title text-left shadowed-text">
+                                Java Developer
+                            </h4>
+                            <h6 className="d-flex opensans-title text-muted">
+                                SUMMER 2016
+                            </h6>
+                            <h5>
+                                Description and responsibilities here!
+                            </h5>
+                        </div>
+
+                        <div className="dropdown-divider my-3">
+                        </div>
+
+                        <div>
+                            <h6 className="pacifico-font">
+                                İm Park Eğitim Bilişim Ltd. Şti.
+                            </h6>
+                            <h4 className="opensans-title text-left shadowed-text">
+                                C# Developer
+                            </h4>
+                            <h6 className="d-flex opensans-title text-muted">
+                                JANUARY 2016 - MAY 2016
+                            </h6>
+                            <h5>
+                                Description and responsibilities here!
+                            </h5>
                         </div>
                     </Fragment>
                 )}/>
