62a00c5ae1f08455411f038f67044bc40cb95378
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2019-04-05 00:26:32 +0300

    Fixed experiences issue

--- src/views/Components/Sections/Experiences.js
+++ src/views/Components/Sections/Experiences.js
@@ -34,74 +34,7 @@ class Experiences extends Component {
             <Fragment>
                 <InfoBlock title={Data["title"]} content={(
                     <Fragment>
-                        <div>
-                            <h6 className="pacifico-font">
-                                Paysera
-                            </h6>
-                            <h4 className="opensans-title text-left shadowed-text">
-                                PhP Developer
-                            </h4>
-                            <h6 className="d-flex opensans-title text-muted">
-                                2018 - Now
-                            </h6>
-                            <h5>
-                                Description and responsibilities here!
-                            </h5>
-                        </div>
-
-                        <div className="dropdown-divider my-3">
-                        </div>
-
-                        <div>
-                            <h6 className="pacifico-font">
-                                BEAM Teknoloji A. Ş.
-                            </h6>
-                            <h4 className="opensans-title text-left shadowed-text">
-                                Web Security Analyst & Secure Developer
-                            </h4>
-                            <h6 className="d-flex opensans-title text-muted">
-                                2017 - 2018
-                            </h6>
-                            <h5>
-                                Description and responsibilities here!
-                            </h5>
-                        </div>
-
-                        <div className="dropdown-divider my-3">
-                        </div>
-
-                        <div>
-                            <h6 className="pacifico-font">
-                                PTT Genel Müdürlüğü
-                            </h6>
-                            <h4 className="opensans-title text-left shadowed-text">
-                                Java Developer
-                            </h4>
-                            <h6 className="d-flex opensans-title text-muted">
-                                SUMMER 2016
-                            </h6>
-                            <h5>
-                                Description and responsibilities here!
-                            </h5>
-                        </div>
-
-                        <div className="dropdown-divider my-3">
-                        </div>
-
-                        <div>
-                            <h6 className="pacifico-font">
-                                İm Park Eğitim Bilişim Ltd. Şti.
-                            </h6>
-                            <h4 className="opensans-title text-left shadowed-text">
-                                C# Developer
-                            </h4>
-                            <h6 className="d-flex opensans-title text-muted">
-                                JANUARY 2016 - MAY 2016
-                            </h6>
-                            <h5>
-                                Description and responsibilities here!
-                            </h5>
-                        </div>
+                        {children}
                     </Fragment>
                 )}/>
             </Fragment>
